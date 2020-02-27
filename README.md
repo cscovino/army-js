48ba5c7f860cbf4f0e43de92df2fd8fe36ed8fa7
Carlos <kikescovino@gmail.com>
2020-02-27 19:04:51 -0300

    complete readme

--- README.md
+++ README.md
@@ -2,20 +2,22 @@
 
 An example to modeling objects with JavaScript.
 
-## Units
+## Definitions
+
+### Units
 
 There are three types of units with different scores:
 - Pikeman   5 points
 - Archer    10 points
 - Knight    20 points
 
-## Armies
+### Armies
 
 An army has an initial quantity of units according with its civilization. It can exist multiples armies from the same civilization.
 An army has 1000 gold coins at the beginning.
 Also each army has its own battles record.
 
-### Civilizations
+#### Civilizations
 
 |            | Pikemen | Archers | Knights |
 |:----------:|:-------:|:-------:|:-------:|
@@ -23,8 +25,61 @@ Also each army has its own battles record.
 |   English  |   10    |   10    |   10    |
 | Byzantines |    5    |    8    |   15    |
 
-### Training
+#### Training
 
-### Transforms
+You can train a specific unit by paying the price according to its type.
 
-### Battles
+|            |Points Earned| Cost |
+|:----------:|:-----------:|:----:|
+|   Pikeman  |      3      |  10  |
+|   Archer   |      7      |  20  |
+|   Knight   |     10      |  30  |
+
+#### Transforms
+
+Also you can transform a specific unit to another by paying the price according to its type.
+
+|Initial Type|Final Type| Cost |
+|:----------:|:--------:|:----:|
+|   Pikeman  |  Archer  |  30  |
+|   Archer   |  Knight  |  40  |
+|   Knight   |    -     |   -  |
+
+#### Battles
+
+An army can attack another at any time. The winner will be the army with more points and the results will be:
+- Winner Army: will earn 100 gold coins.
+- Loser Army: will lose the two units with more points.
+- Draw: both army will lose the two units with more points.
+
+## Usage
+
+First of all you need to import the module (asumming that you are inside the folder of the project):
+```
+const armyjs = require('./army.js')
+```
+
+Then to create an army you need to run:
+```
+const army = new armyjs.Army('Chinese')
+const enemy = new armyjs.Army('Byzantines')
+```
+If you print the object army you will see the attributes of that object.
+
+To train a specific unit you have to pass the id of the unit to train to the train function, for example here we trained the `Pikeman 1` of the chinese army:
+```
+army.train(1)
+```
+Now if you print again the army object you will see that the attributes gold and the points of the unit have change according to the type of the unit.
+
+To transform a unit to another unit type you have to pass the id of the unit to transform, for example here we transformed the `Pikeman 1`:
+```
+army.transform(1)
+```
+Again if you print the army object you will see that the attributes gold and the type of the unit have change.
+
+Finally to attack other army you just have to pass the enemy army object to the battle function, for example here `army` army attacked `enemy` army:
+```
+army.battle(enemy)
+```
+And if you print the objects you will see that gold, records and units have change following the rules explained.
--- army.js
+++ army.js
@@ -27,6 +27,119 @@ function _getIndexMaxUnit(units){
     return tmp.indexOf(maxPoints);
 }
 
+function _trainUnit(unit){
+    switch (unit.type) {
+        case "Knight":
+            unit.points += 10;
+            console.log("[INFO] "+unit.name+" has now 10 points more");
+            break;
+        case "Archer":
+            unit.points += 7;
+            console.log("[INFO] "+unit.name+" has now 7 points more");
+            break;
+        case "Pikeman":
+            unit.points += 3;
+            console.log("[INFO] "+unit.name+" has now 3 points more");
+            break;
+    }
+}
+
+function _transformUnit(unit){
+    switch (unit.type) {
+        case "Knight":
+            console.log("[ERROR] A Knight can not execute a transform");
+            break;
+        case "Archer":
+            unit.type = "Knight";
+            console.log("[INFO] "+unit.name+" is now a Knight");
+            break;
+        case "Pikeman":
+            unit.type = "Archer";
+            console.log("[INFO] "+unit.name+" is now an Archer");
+            break;
+    }
+}
+
+function _trainArmy(army, unitID){
+    const unit = army.units.filter(unit => unit.id === unitID);
+    unit[0].train();
+    switch (unit[0].type) {
+        case "Knight":
+            army.gold -= 30;
+            console.log(`[INFO] ${army.name} has now 30 gold less`);
+            break;
+        case "Archer":
+            army.gold -= 20;
+            console.log(`[INFO] ${army.name} has now 20 gold less`);
+            break;
+        case "Pikeman":
+            army.gold -= 10;
+            console.log(`[INFO] ${army.name} has now 10 gold less`);
+            break;
+    }
+}
+
+function _transformArmy(army, unitID){
+    const unit = army.units.filter(unit => unit.id === unitID);
+    switch (unit[0].type) {
+        case "Knight":
+            console.log("[ERROR] A Knight can not execute a transform");
+            break;
+        case "Archer":
+            army.gold -= 40;
+            unit[0].transform();
+            console.log(`[INFO] ${army.name} has now 40 gold less`);
+            break;
+        case "Pikeman":
+            army.gold -= 30;
+            unit[0].transform();
+            console.log(`[INFO] ${army.name} has now 30 gold less`);
+            break;
+    }
+}
+
+function _battle(army, enemy, loses=2, losesDraw=2){
+    if(army instanceof Army && enemy instanceof Army){
+        console.log(`[INFO] There was a Battle between ${army.name} and ${enemy.name}`);
+        const armyPoints1 = _getPoints(army);
+        const armyPoints2 = _getPoints(enemy);
+        if (armyPoints1 === armyPoints2) {
+            let lostArmy1 = []; 
+            let lostArmy2 = [];
+            for (let index = 0; index < losesDraw; index++) {
+                const indexArmy1 = _getIndexMaxUnit(army.units);
+                const indexArmy2 = _getIndexMaxUnit(enemy.units);
+                lostArmy1 = lostArmy1.concat(army.units.splice(indexArmy1,1));
+                lostArmy2 = lostArmy2.concat(enemy.units.splice(indexArmy2,1));
+            }
+            army.records = army.records.concat(new Record("Draw", lostArmy1, enemy.name));
+            enemy.records = enemy.records.concat(new Record("Draw", lostArmy2, army.name));
+            console.log("[INFO] Draw!");
+        } else if (armyPoints1 > armyPoints2) {
+            let lostArmy2 = [];
+            for (let index = 0; index < loses; index++) {
+                const indexArmy2 = _getIndexMaxUnit(enemy.units);
+                lostArmy2 = lostArmy2.concat(enemy.units.splice(indexArmy2,1));
+            }
+            army.gold += 100;
+            army.records = army.records.concat(new Record("Victory", [], enemy.name));
+            enemy.records = enemy.records.concat(new Record("Lost", lostArmy2, army.name));
+            console.log(`[INFO] ${army.name} won!`);        
+        } else {
+            let lostArmy1 = []; 
+            for (let index = 0; index < losesDraw; index++) {
+                const indexArmy1 = _getIndexMaxUnit(army.units);
+                lostArmy1 = lostArmy1.concat(army.units.splice(indexArmy1,1));
+            }
+            army.records = army.records.concat(new Record("Lost", lostArmy1, enemy.name));
+            enemy.gold += 100;
+            enemy.records = enemy.records.concat(new Record("Victory", [], army.name)); 
+            console.log(`[INFO] ${enemy.name} won!`);           
+        }
+    }
+    else console.log("[ERROR] A Battle needs 2 armies");
+}
+
 //////////////////////////////////// MODELS ///////////////////////////////////////////
 function Unit(type="Unit"){
     this.id = unitCounter;
@@ -46,138 +159,35 @@ function Unit(type="Unit"){
             this.type = "Pikeman"
             break;
     }
-    this.train = () => {
-        switch (this.type) {
-            case "Knight":
-                this.points += 10;
-                console.log("[INFO] "+this.name+" has now 10 points more");
-                break;
-            case "Archer":
-                this.points += 7;
-                console.log("[INFO] "+this.name+" has now 7 points more");
-                break;
-            case "Pikeman":
-                this.points += 3;
-                console.log("[INFO] "+this.name+" has now 3 points more");
-                break;
-        }
-    }
-    this.transform = () => {
-        switch (this.type) {
-            case "Knight":
-                console.log("[ERROR] A Knight can not execute a transform");
-                break;
-            case "Archer":
-                this.type = "Knight";
-                console.log("[INFO] "+this.name+" is now a Knight");
-                break;
-            case "Pikeman":
-                this.type = "Archer";
-                console.log("[INFO] "+this.name+" is now an Archer");
-                break;
-        }
-    }
+    this.train = () => _trainUnit(this);
+    this.transform = () => _transformUnit(this);
     console.log(`[INFO] ${this.type==="Archer"?"An":"A"} ${this.type} was created`);
     unitCounter++;
 }
 
-function Army(civil="Civilization"){
+function Army(civilization="Civilization"){
     this.id = armyCounter;
-    this.name = civil + " " + this.id.toString();
+    this.name = civilization + " " + this.id.toString();
     this.gold = 1000;
     this.records = [];
-    switch (civil.toLowerCase()) {
+    switch (civilization.toLowerCase()) {
         case "byzantines":
             this.units = _createArmy(5,8,15);
-            this.civil = "Byzantines"
+            this.civilization = "Byzantines"
             break;
         case "english":
             this.units = _createArmy(10,10,10);
-            this.civil = "English"
+            this.civilization = "English"
             break;
         case "chinese":
         default:
             this.units = _createArmy(2,25,2);
-            this.civil = "Chinese"
+            this.civilization = "Chinese"
             break;
     }
-    this.train = (id) => {
-        const unit = this.units.filter(unit => unit.id === id);
-        unit[0].train();
-        switch (unit[0].type) {
-            case "Knight":
-                this.gold -= 30;
-                console.log(`[INFO] ${this.name} has now 30 gold less`);
-                break;
-            case "Archer":
-                this.gold -= 20;
-                console.log(`[INFO] ${this.name} has now 20 gold less`);
-                break;
-            case "Pikeman":
-                this.gold -= 10;
-                console.log(`[INFO] ${this.name} has now 10 gold less`);
-                break;
-        }
-    }
-    this.transform = (id) => {
-        const unit = this.units.filter(unit => unit.id === id);
-        switch (unit[0].type) {
-            case "Knight":
-                console.log("[ERROR] A Knight can not execute a transform");
-                break;
-            case "Archer":
-                this.gold -= 40;
-                unit[0].transform();
-                console.log(`[INFO] ${this.name} has now 40 gold less`);
-                break;
-            case "Pikeman":
-                this.gold -= 30;
-                unit[0].transform();
-                console.log(`[INFO] ${this.name} has now 30 gold less`);
-                break;
-        }
-    }
-    this.battle = (enemy, loses=2, losesDraw=2) => {
-        if(enemy instanceof Army){
-            console.log(`[INFO] There was a Battle between ${this.name} and ${enemy.name}`);
-            const armyPoints1 = _getPoints(this);
-            const armyPoints2 = _getPoints(enemy);
-            if (armyPoints1 === armyPoints2) {
-                let lostArmy1 = []; 
-                let lostArmy2 = [];
-                for (let index = 0; index < losesDraw; index++) {
-                    const indexArmy1 = _getIndexMaxUnit(this.units);
-                    const indexArmy2 = _getIndexMaxUnit(enemy.units);
-                    lostArmy1 = lostArmy1.concat(this.units.splice(indexArmy1,1));
-                    lostArmy2 = lostArmy2.concat(enemy.units.splice(indexArmy2,1));
-                }
-                this.records = this.records.concat(new Record("Draw", lostArmy1, enemy.name));
-                enemy.records = enemy.records.concat(new Record("Draw", lostArmy2, this.name));
-                console.log("[INFO] Draw!");
-            } else if (armyPoints1 > armyPoints2) {
-                let lostArmy2 = [];
-                for (let index = 0; index < loses; index++) {
-                    const indexArmy2 = _getIndexMaxUnit(enemy.units);
-                    lostArmy2 = lostArmy2.concat(enemy.units.splice(indexArmy2,1));
-                }
-                this.gold += 100;
-                this.records = this.records.concat(new Record("Victory", [], enemy.name));
-                enemy.records = enemy.records.concat(new Record("Lost", lostArmy2, this.name));
-                console.log(`[INFO] ${this.name} won!`);        
-            } else {
-                let lostArmy1 = []; 
-                for (let index = 0; index < losesDraw; index++) {
-                    const indexArmy1 = _getIndexMaxUnit(this.units);
-                    lostArmy1 = lostArmy1.concat(this.units.splice(indexArmy1,1));
-                }
-                this.records = this.records.concat(new Record("Lost", lostArmy1, enemy.name));
-                enemy.gold += 100;
-                enemy.records = enemy.records.concat(new Record("Victory", [], this.name)); 
-                console.log(`[INFO] ${enemy.name} won!`);           
-            }
-        }
-        else console.log("[ERROR] A Battle needs 2 armies");
-    }
+    this.train = (id) => _trainArmy(this, id);
+    this.transform = (id) => _transformArmy(this, id);
+    this.battle = (enemy) => _battle(this, enemy);
     armyCounter++;
 }
 
